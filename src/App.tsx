3384591379b53879bf70105cb19f1893bf44e58d
bobberg90 <bobberg90@hotmail.com>
2024-05-29 14:09:23 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  // URLs of the audio clips
+  const sounds = [
+    {
+      name: "Jeroen niet doen!",
+      url: "./jeroen-niet-doen.mp3",
+      img: "./images/jeroen-niet-doen.png",
+    },
+    {
+      name: "We are Champions League, not Keukenkampioen",
+      url: "path/to/sound2.mp3",
+      img: "./images/we-are-champions-league-not-keukenkampioen.png",
+    },
+    {
+      name: "Priority no. 1 remains, continue business as usual",
+      url: "path/to/sound3.mp3",
+      img: "./images/priority-no-1-remains-continue-business-as-usual.png",
+    },
+    {
+      name: "Are we going to Dickies?",
+      url: "path/to/sound4.mp3",
+      img: "./images/are-we-going-to-dickies.png",
+    },
+    {
+      name: "I don't see it on the slide, so it's not there...",
+      url: "path/to/sound5.mp3",
+      img: "./images/i-dont-see-it-in-the-slide-so-its-not-there.png",
+    },
+    {
+      name: "How many clients did you call today?",
+      url: "path/to/sound5.mp3",
+      img: "./images/how-many-clients-did-you-call-today.png",
+    },
+    {
+      name: "Let's get real or let's not play",
+      url: "path/to/sound5.mp3",
+      img: "./images/lets-get-real-or-lets-not-play.png",
+    },
+    {
+      name: "That is NOT a good question",
+      url: "path/to/sound5.mp3",
+      img: "./images/that-is-not-a-good-question.png",
+    },
+    {
+      name: "I'm not gonna give you answers, only context",
+      url: "path/to/sound5.mp3",
+      img: "./images/im-not-gonna-give-you-answers-only-context.png",
+    },
+    {
+      name: "Consulting is a marathon, not a sprint",
+      url: "path/to/sound5.mp3",
+      img: "./images/consulting-is-a-marathon-not-a-sprint.png",
+    },
+    {
+      name: "If you don't follow the process, it doesn't exist",
+      url: "path/to/sound5.mp3",
+      img: "./images/if-you-dont-follow-the-process-it-doesnt-exist.png",
+    },
+  ];
+
+  const playSound = (url: string) => {
+    const audio = new Audio(url);
+    audio.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>The Jeroen Soundboard</h1>
+      {/* add image of jeroen */}
+      <img style={{ height: "200px" }} src="/images/jeroen.png" alt="" />
+      <br />
+      {sounds.map((sound, index) => (
+        <img
+          key={index}
+          style={buttonStyle}
+          onClick={() => playSound(sound.url)}
+          src={sound.img}
+          alt={sound.name}
+        ></img>
+      ))}
     </div>
   );
 }
+
+// Style for the buttons
+const buttonStyle = {
+  margin: "10px",
+  padding: "10px 20px",
+  fontSize: "16px",
+  cursor: "pointer",
+  height: "200px",
+};
 
 export default App;
